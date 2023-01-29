7e3cf8019e59bd3542567388a61cf0e6a1455b4b
Popular Toppling Jelly <81684355+PopularTopplingJelly@users.noreply.github.com>
2023-01-29 23:29:29 +0100

    nvm

--- satellite/chatroomsSatellite.php
+++ satellite/chatroomsSatellite.php
@@ -21,20 +21,20 @@ class Chatroom implements MessageComponentInterface {
 	public function __construct() {
 		echo("Welcome to the Chatrooms Experience! (Compiled on 29/01/2023)\n
       Chatrooms is a free, open source and lightweight chat platform where anyone can host a space for their friends, people and even family to hang out.
-    Copyright (C) 20". date("y") ."  Team CloudSeeker (Popular Toppling Jelly/Teodor Boshkoski and GeofTheCake)\n\n
+    Copyright (C) 20". date("y") ."  Team CloudSeeker (Popular Toppling Jelly/Teodor Boshkoski and GeofTheCake)\n
 
-    This program is free software: you can redistribute it and/or modify\n
-    it under the terms of the GNU General Public License as published by\n
-    the Free Software Foundation, either version 3 of the License, or\n
-    (at your option) any later version.\n\n
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.\n
 
-    This program is distributed in the hope that it will be useful,\n
-    but WITHOUT ANY WARRANTY; without even the implied warranty of\n
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n
-    GNU General Public License for more details.\n\n
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.\n
 
-    You should have received a copy of the GNU General Public License\n
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.\n\n");
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.\n");
 		$this->clients = new \SplObjectStorage;
 		echo("[Satellite] Server started\n");
 		sleep(5);
