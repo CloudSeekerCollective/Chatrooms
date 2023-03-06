c58bbd4cebac4a66313c6af3c96f47bd27cc4c8b
Popular Toppling Jelly <81684355+PopularTopplingJelly@users.noreply.github.com>
2023-03-06 22:45:08 +0100

    Update chatroomsSatellite.php

--- satellite/chatroomsSatellite.php
+++ satellite/chatroomsSatellite.php
@@ -492,7 +492,7 @@ class Chatroom implements MessageComponentInterface {
 									}
 								}
 								foreach($lfdc_RSLT as $channel) {
-									$from->send('{"action":"channel","status":"success", "name":"'. $nm .'", "id":"'. $id .'"}');
+									$from->send('{"action":"channel","status":"success", "name":"'. stripslashes(htmlspecialchars($channel['name'])) .'", "id":"'. stripslashes(htmlspecialchars($channel['id'])) .'"}');
 								}
 							}
 							else{
