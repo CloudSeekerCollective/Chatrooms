b40e909d0a2e614a0ab556f8c318d18395d41e02
Popular Toppling Jelly <81684355+PopularTopplingJelly@users.noreply.github.com>
2023-04-05 23:11:54 +0200

    lets do this too

--- satellite/chatroomsSatellite.php
+++ satellite/chatroomsSatellite.php
@@ -564,7 +564,65 @@ class Chatroom implements MessageComponentInterface {
 								$cdate = stripslashes(htmlspecialchars(gmdate("F nS Y, G:i", $lfdu_RSLT['creationdate'])));
 								// COMING SOON: $lldate = stripslashes(htmlspecialchars(gm_date($lfdu_RSLT['lastlogindate'])));
 								// return user info
-								$from->send('{"action":"user", "status":"success", "username":"'. $usrnm .'", "id":"'. $actualuid .'", "status":"'. $stts .'", "picture":"'. $pfp .'", "profilestatus":"'. $ustts .'", "creationDate":"'. $cdate .'"}');
+								$from->send('{"action":"user", "xstatus":"success", "username":"'. $usrnm .'", "id":"'. $actualuid .'", "status":"'. $stts .'", "picture":"'. $pfp .'", "profilestatus":"'. $ustts .'", "creationDate":"'. $cdate .'"}');
+								echo(json_encode(array("username" => $usrnm, "id" => $actualuid, "status" => $stts, "picture" => $pfp, "profilestatus" => $ustts, "creationDate" => $cdate/*, "lastLoginDate" => $lldate*/)));
+							}
+							// otherwise...
+							else{
+								// return warning telling the client that the user is unexistent
+								$from->send('{"action":"user", "status":"xfail"}');
+							}	
+						}
+					}
+				}
+			}
+		}
+	}
+			break;
+			case "account":
+				if(!empty($dataset['authentication'])){
+		// isolate authentication
+		$auth = stripslashes(htmlspecialchars($dataset['authentication']));
+
+		// lfdu = look for da user
+		$security_lfdu = mysqli_query($ctds, "SELECT `username`, `id` FROM `accounts` WHERE `authentication`='". $auth ."'");
+			
+		// if the result is NOT a boolean (in other words an error)...
+		if(!is_bool($security_lfdu)){
+			// if the authentication matches a user...
+			if(mysqli_num_rows($security_lfdu) != 0){
+				// isolate authentication
+				$auth = stripslashes(htmlspecialchars($dataset['authentication']));	
+
+				// lfdu = look for da user
+				$security_lfdu = mysqli_query($ctds, "SELECT `username`, `id` FROM `accounts` WHERE `authentication`='". $auth ."'");
+					
+				// if the result is NOT a boolean (in other words an error)...
+				if(!is_bool($security_lfdu)){
+					// if the authentication matches a user...
+					if(mysqli_num_rows($security_lfdu) != 0){
+						// isolate the user ID
+						$uid = stripslashes(htmlspecialchars($dataset['authentication']));
+		
+						// lfdu = look for da user
+						$lfdu = mysqli_query($ctds, "SELECT `username`, `picture`, `id`, `status`, `profilestatus`, `creationdate` FROM `accounts` WHERE `authentication`='". $uid ."'");
+						
+						// if there is no error...
+						if(!is_bool($lfdu)){
+							// if a user matching the ID exists...
+							if(mysqli_num_rows($lfdu) != 0){
+								// cache db result
+								$lfdu_RSLT = mysqli_fetch_assoc($lfdu);
+								// isolate all of these variables JUST IN CASE
+								$usrnm = stripslashes(htmlspecialchars($lfdu_RSLT['username']));
+								$actualuid = stripslashes(htmlspecialchars($lfdu_RSLT['id']));
+								$stts = stripslashes(htmlspecialchars($lfdu_RSLT['status']));
+								$pfp = stripslashes(htmlspecialchars($lfdu_RSLT['picture']));
+								$ustts = "". stripslashes(htmlspecialchars($lfdu_RSLT['profilestatus']));
+								$cdate = stripslashes(htmlspecialchars(gmdate("F nS Y, G:i", $lfdu_RSLT['creationdate'])));
+								// COMING SOON: $lldate = stripslashes(htmlspecialchars(gm_date($lfdu_RSLT['lastlogindate'])));
+								// return user info
+								$from->send('{"action":"user", "xstatus":"success", "username":"'. $usrnm .'", "id":"'. $actualuid .'", "status":"'. $stts .'", "picture":"'. $pfp .'", "profilestatus":"'. $ustts .'", "creationDate":"'. $cdate .'"}');
 								echo(json_encode(array("username" => $usrnm, "id" => $actualuid, "status" => $stts, "picture" => $pfp, "profilestatus" => $ustts, "creationDate" => $cdate/*, "lastLoginDate" => $lldate*/)));
 							}
 							// otherwise...
