d7eb7cb54dd4de836ed19129da5c4ebdb97fb614
Popular Toppling Jelly <81684355+PopularTopplingJelly@users.noreply.github.com>
2023-05-24 12:18:00 +0200

    preparation for 0.7.3

--- satellite/chatroomsSatellite.php
+++ satellite/chatroomsSatellite.php
@@ -631,7 +631,7 @@ class Chatroom implements MessageComponentInterface {
 						"allow_registrations":"'. $serverconfig['allow_registrations'] .'",
 						"filesize_limit":"'. $serverconfig['filesize_limit'] .'",
 						"chatrooms_distro":"'. $serverconfig['chatrooms_distro'] .'",
-						"satellite_version":"0.7.22",
+						"satellite_version":"0.7.3",
 						"emotes":'. json_encode($serverconfig['emotes']) .'}');
 				}
 				catch(Exception $e){
